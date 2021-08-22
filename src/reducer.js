6be6c76645076dbc1c2550901304cb282b9471f9
RawandDev <rawand.200091384037@gmail.com>
2021-08-22 21:54:23 +0300

    done!

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,24 @@
 // add code snippets from README
+
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+function render() {
+  const container = document.getElementById("container");
+  container.innerHTML = state.count;
+}
+
+dispatch({type: '@@INIT'})
